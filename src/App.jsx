cc83affb3aa7c19dea9af56e4d6ebb0979c136a9
GAURIKOPARKAR <gaurikoparkar@gmail.com>
2024-03-28 21:43:03 +0530

    404.html created

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,30 @@
 import React from 'react';
-import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './Components/Home';
 import About from './Components/About';
 import Navbar from './Components/Navbar';
 import Contact from './Components/Contact';
 import Footer from './Components/Footer';
 
-
 const App = () => {
   const router = createBrowserRouter([
-   { path: "/",
-    element:<><Navbar/><Home/></>
-  },
-  { path: "/About",
-    element:<><Navbar/><About/></>
-  },
-  { path: "/Contact",
-    element:<><Navbar/><Contact/></>  }
-
-
-  ])
+    { path: "/",
+      element: <><Navbar /><Home /></>
+    },
+    { path: "/About",
+      element: <><Navbar /><About /></>
+    },
+    { path: "/Contact",
+      element: <><Navbar /><Contact /></>  
+    }
+  ]);
 
   return (
     <>
-     <div className='bg-[url("./public/Images/grid.avif")] h-screen w-screen'>
-     <RouterProvider router={router}></RouterProvider> 
-     </div>
-      <Footer></Footer>
+         <div className='bg-[url("./public/Images/grid.avif")] h-screen w-screen'>
+        <RouterProvider router={router}></RouterProvider>
+      </div>
+      <Footer />
     </>
   );
 };
