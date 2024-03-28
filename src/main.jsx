8acbf764919169baf214f9a6eb59c597f459c00c
GAURIKOPARKAR <gaurikoparkar@gmail.com>
2024-03-28 21:49:46 +0530

    Routering move to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Navbar from './Components/Navbar.jsx'
+import Home from './Components/Home.jsx'
+import About from './Components/About.jsx'
+import Contact from './Components/Contact.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+const router = createBrowserRouter([
+  { path: "/",
+    element: <><Navbar /><Home /></>
+  },
+  { path: "/About",
+    element: <><Navbar /><About /></>
+  },
+  { path: "/Contact",
+    element: <><Navbar /><Contact /></>  
+  }
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+     <div className='bg-[url("./public/Images/grid.avif")] h-screen w-screen'>
+        
+       
+     <RouterProvider router={router}></RouterProvider>
+     </div>
     <App />
+   
   </React.StrictMode>,
 )
