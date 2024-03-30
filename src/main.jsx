6ca7a53004cd0b668e9208babb52db43aa812dc5
GAURIKOPARKAR <gaurikoparkar@gmail.com>
2024-03-31 00:57:06 +0530

    firebase code added in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
@@ -7,6 +9,17 @@ import Home from './Components/Home.jsx'
 import About from './Components/About.jsx'
 import Contact from './Components/Contact.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+const firebaseConfig = {
+  apiKey: "AIzaSyBxOvcIKy4pS4OXrxE5FR9yn3ZHM9JC7zQ",
+  authDomain: "excel-made-easy.firebaseapp.com",
+  projectId: "excel-made-easy",
+  storageBucket: "excel-made-easy.appspot.com",
+  messagingSenderId: "807319047426",
+  appId: "1:807319047426:web:20c988a80847f81766061b",
+  measurementId: "G-P6PEKJ04ZB"
+};
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const router = createBrowserRouter([
   { path: "/",
