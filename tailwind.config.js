8b2c20fe0e12dffd4abb0342c84ebd6300780052
GAURIKOPARKAR <gaurikoparkar@gmail.com>
2024-03-31 15:19:52 +0530

    Image updated in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ export default {
   ],
   theme: {
     extend: {  backgroundImage: {
-      'hero': "url('./Images/grid.avif')"
+      'hero': "url('./Images/logo.png')"
         }},
   },
   plugins: [],
