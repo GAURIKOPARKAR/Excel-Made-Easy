15cf63379afcc7136a2ef3d073ff3dc36f46390a
GAURIKOPARKAR <gaurikoparkar@gmail.com>
2024-06-09 20:20:37 +0530

    GST Billing point added in services

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,6 +5,7 @@ const Home = () => {
     <div className='border rounded-2xl m-5 p-8 bg-white'>
       <h2 className='text-3xl font-bold text-center mb-6'>Services Provided by Us ....</h2>
       <ul className='text-lg text-gray-700 list-disc ml-8'>
+        <li className='mb-4'>GST Billing Softwares</li>
         <li className='mb-4'>Multiple Software in Excel</li>
         <li className='mb-4'>Data Entry and Label Creation</li>
         <li className='mb-4'>Advanced Formulas</li>
